8d57b7480f7b533d58230753ffd3bafa3cb069a1
steven14523 <darguello00876@ufide.ac.cr>
2021-04-02 10:57:51 -0600

    menu

--- Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
+++ Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
@@ -1,10 +1,15 @@
+
+import javax.swing.JOptionPane;
+
 //este sera donde se muestre el menu para ordenar los helados
 public class CatalogoOrdenar {
     
     
     int oprtion = 0;
     do  
-    opcion 
+    opcion = Interguer.ParseInt(JOptionPane.showInputDialog(
+                        "Bienvenido a Dulce Tropic"
+                        )
     
 }
     
