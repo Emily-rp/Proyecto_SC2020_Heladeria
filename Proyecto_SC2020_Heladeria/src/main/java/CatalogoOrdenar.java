92c3073b0f746d37bd8c572f1d4b10cc7887a5ce
steven14523 <darguello00876@ufide.ac.cr>
2021-04-21 17:54:08 -0600

    Update CatalogoOrdenar.java

--- Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
+++ Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
@@ -13,7 +13,16 @@ public class CatalogoOrdenar {
                                         + "5.salir\n");    
                               opcion=Integer.parseInt(op);
                               switch(opcion){
-                                  case 1: JOptionPane.showMessageDialog(null,"Escoge tu helado. opcion"); break; 
+                                  case 1: JOptionPane.showMessageDialog(null,"Escoge tu helado. "); 
+                                     op=JOptionPane.showInputDialog("Escoge el sabor de tu helado a tú prefencia:\n"
+                                             +"1- Helado m&m\n"
+                                             +"2- Helado Oreo\n"
+                                             +"3- Helado Chicle\n"
+                                             +"4-Helado light\n"
+                                             +"5-Arma a tu gusto\n"
+                                             +"6-Vuelve al menú Principal\n");
+                                  
+                                  
                                   case 2: JOptionPane.showInternalMessageDialog(null, "Escoge un Sunday a tu preferencia"); break;
                                   case 3: JOptionPane.showMessageDialog(null, "Escoge el Sabor de Crepa"); break;
                                   case 4: JOptionPane.showInternalMessageDialog(null, "Escoge el sabor de tu cono"); break;
