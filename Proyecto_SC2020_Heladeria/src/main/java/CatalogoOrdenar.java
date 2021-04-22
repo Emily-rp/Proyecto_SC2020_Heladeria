35ee1448a4c7efd6db383ea9300eb12dff052317
steven14523 <darguello00876@ufide.ac.cr>
2021-04-21 21:25:59 -0600

    Update CatalogoOrdenar.java

--- Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
+++ Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
@@ -16,19 +16,17 @@ public class CatalogoOrdenar {
                               opcion=Integer.parseInt(op);
                               switch(opcion){
                                   case 1: JOptionPane.showMessageDialog(null,"Escoge tu helado. "); 
-                                     op=JOptionPane.showInputDialog("Escoge el sabor de tu helado a tú prefencia:\n"
+                                  
+                                   int numero1 = Integer  .parseInt(JOptionPane.showInputDialog("Escoge el sabor de tu helado a tú prefencia:\n"
                                              +"1- Helado m&m ₡2000 \n"
                                              +"2- Helado Oreo ₡2000\n"
                                              +"3- Helado Chicle ₡2000\n"
-                                             +"4-Helado light ₡2500\n"
-                                              +"5-Vuelve al menú Principal\n");
+                                             +"4-Helado light ₡2000\n"
+                                              +"5-Vuelve al menú Principal\n"));
                                              
-                                     if( a == 1){    
-                                        a = 2000;
-                                        if (a==1){
-                                            a =2000;
-                                            }else if (a==2) {
-                                                      a= 3000;
+                                   if (numero1 == 1) {
+                                        numero1 = 2000;
+                                            JOptionPane.showMessageDialog(null, "Su precio es de 2000");
                                         
                                      }
                                      
@@ -42,21 +40,33 @@ public class CatalogoOrdenar {
                                   
                                   
                                   case 2: JOptionPane.showInternalMessageDialog(null, "Escoge un Sunday a tu preferencia");
-                                        op=JOptionPane.showInputDialog("Escoge entre diferentes sabores tú Sundae:\n"
+                                        int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Escoge entre diferentes sabores tú Sundae:\n"
                                                 + "1.Fresa ₡2000\n"
                                                 + "2.vainilla ₡2000\n"
                                                 + "3.chocolate ₡2000\n"
                                                 + "4.caramelo ₡2000\n"
-                                                + "5.Salir");
+                                                + "5.Salir"));
+                                        if (numero2 == 2) {
+                                        numero2 = 2000;
+                                            JOptionPane.showMessageDialog(null, "Su precio es de 2000");
                                                 
                                   
                                   case 3: JOptionPane.showMessageDialog(null, "Escoge el Sabor de Crepa"); 
-                                    op=JOptionPane.showInputDialog("Escoge entre diferentes sabores de crepas:\n"
+                                   int numero3 = Integer.parseInt(JOptionPane.showInputDialog("Escoge entre diferentes sabores de crepas:\n"
                                             +"1- Dulce tentación ₡3000\n"
                                             +"2- frutos del bosque ₡3000\n"
                                             +"3- Manzana ₡2000\n" 
                                             +"4- Caribean Style ₡2500\n"
-                                            +"5- Volver al menú\n");
+                                            +"5- Volver al menú\n"));
+                                            if (numero3 == 3) {
+                                        numero3 = 2000;
+                                            JOptionPane.showMessageDialog(null, "Su precio es de 2000");
+                                            if (numero1 == 1) {
+                                                 numero1 = 2000;
+                                            JOptionPane.showMessageDialog(null, "Su precio es de 2000");
+                                                    } else if (numero1 == 2) {
+                                                                b = 3000;
+                                            JOptionPane.showMessageDialog(null, "Su precio es de "3000);
                                             
                                   case 4: JOptionPane.showInternalMessageDialog(null, "Escoge el sabor de tu cono"); 
                                     op=JOptionPane.showInputDialog("Que sabor deseas tu Cono\n"
@@ -73,3 +83,4 @@ public class CatalogoOrdenar {
     
     
 }
+    
