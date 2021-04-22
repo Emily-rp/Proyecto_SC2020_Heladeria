60256f3b4ac8141a9c18ff561299d0536f54e655
steven14523 <darguello00876@ufide.ac.cr>
2021-04-21 20:29:44 -0600

    Update CatalogoOrdenar.java

--- Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
+++ Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
@@ -21,10 +21,33 @@ public class CatalogoOrdenar {
                                              +"4-Helado light\n"
                                              +"5-Arma a tu gusto\n"
                                              +"6-Vuelve al menú Principal\n");
+                                     
+                                         
+
+                                             
+                                             
+                                         
+                                     
+                                    
                                   
                                   
-                                  case 2: JOptionPane.showInternalMessageDialog(null, "Escoge un Sunday a tu preferencia"); break;
-                                  case 3: JOptionPane.showMessageDialog(null, "Escoge el Sabor de Crepa"); break;
+                                  case 2: JOptionPane.showInternalMessageDialog(null, "Escoge un Sunday a tu preferencia");
+                                        op=JOptionPane.showInputDialog("Escoge entre diferentes sabores tú Sunday:\n"
+                                                + "1.Fresa\n"
+                                                + "2.vainilla\n"
+                                                + "3.chocolate\n"
+                                                + "4.caramelo\n"
+                                                + "5.Salir");
+                                                
+                                  
+                                  case 3: JOptionPane.showMessageDialog(null, "Escoge el Sabor de Crepa"); 
+                                    op=JOptionPane.showInputDialog("Escoge entre diferentes sabores de crepas:\n"
+                                            +"1- Dulce tentación\n"
+                                            +"2- frutos del bosque\n"
+                                            +"3- Manzana\n" 
+                                            +"4- Caribean Style\n"
+                                            +"5- Volver al menú\n");
+                                            
                                   case 4: JOptionPane.showInternalMessageDialog(null, "Escoge el sabor de tu cono"); break;
                                   case 5: JOptionPane.showInternalMessageDialog(null, "Salir del menú"); break;  }   
     }
