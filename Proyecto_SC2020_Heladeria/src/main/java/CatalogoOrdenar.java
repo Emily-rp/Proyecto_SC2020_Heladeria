69634e638400d0e0b35424a20f510803eaf2cb9d
steven14523 <darguello00876@ufide.ac.cr>
2021-04-21 20:54:12 -0600

    menu

--- Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
+++ Proyecto_SC2020_Heladeria/src/main/java/CatalogoOrdenar.java
@@ -1,13 +1,15 @@
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 //este sera donde se muestre el menu para ordenar los helados
 public class CatalogoOrdenar {
     public static void main(String[]args){
-        int opcion=0; String op="";
+        int opcion=0; String op=""; 
+        Scanner LeerPrecios= new Scanner(System.in); int a = 0;
             op=JOptionPane.showInputDialog("ESCOGE A TU PREFERENCIA:\n"
                                         + "1.Helado \n"
-                                        + "2.Sunday\n"
+                                        + "2.Sundae\n"
                                         + "3.Crepas\n"
                                         + "4.Cono\n"
                                         + "5.salir\n");    
@@ -15,12 +17,20 @@ public class CatalogoOrdenar {
                               switch(opcion){
                                   case 1: JOptionPane.showMessageDialog(null,"Escoge tu helado. "); 
                                      op=JOptionPane.showInputDialog("Escoge el sabor de tu helado a tú prefencia:\n"
-                                             +"1- Helado m&m\n"
-                                             +"2- Helado Oreo\n"
-                                             +"3- Helado Chicle\n"
-                                             +"4-Helado light\n"
-                                             +"5-Arma a tu gusto\n"
-                                             +"6-Vuelve al menú Principal\n");
+                                             +"1- Helado m&m ₡2000 \n"
+                                             +"2- Helado Oreo ₡2000\n"
+                                             +"3- Helado Chicle ₡2000\n"
+                                             +"4-Helado light ₡2500\n"
+                                              +"5-Vuelve al menú Principal\n");
+                                             
+                                     if( a == 1){    
+                                        a = 2000;
+                                        if (a==1){
+                                            a =2000;
+                                            }else if (a==2) {
+                                                      a= 3000;
+                                        
+                                     }
                                      
                                          
 
@@ -32,29 +42,29 @@ public class CatalogoOrdenar {
                                   
                                   
                                   case 2: JOptionPane.showInternalMessageDialog(null, "Escoge un Sunday a tu preferencia");
-                                        op=JOptionPane.showInputDialog("Escoge entre diferentes sabores tú Sunday:\n"
-                                                + "1.Fresa\n"
-                                                + "2.vainilla\n"
-                                                + "3.chocolate\n"
-                                                + "4.caramelo\n"
+                                        op=JOptionPane.showInputDialog("Escoge entre diferentes sabores tú Sundae:\n"
+                                                + "1.Fresa ₡2000\n"
+                                                + "2.vainilla ₡2000\n"
+                                                + "3.chocolate ₡2000\n"
+                                                + "4.caramelo ₡2000\n"
                                                 + "5.Salir");
                                                 
                                   
                                   case 3: JOptionPane.showMessageDialog(null, "Escoge el Sabor de Crepa"); 
                                     op=JOptionPane.showInputDialog("Escoge entre diferentes sabores de crepas:\n"
-                                            +"1- Dulce tentación\n"
-                                            +"2- frutos del bosque\n"
-                                            +"3- Manzana\n" 
-                                            +"4- Caribean Style\n"
+                                            +"1- Dulce tentación ₡3000\n"
+                                            +"2- frutos del bosque ₡3000\n"
+                                            +"3- Manzana ₡2000\n" 
+                                            +"4- Caribean Style ₡2500\n"
                                             +"5- Volver al menú\n");
                                             
                                   case 4: JOptionPane.showInternalMessageDialog(null, "Escoge el sabor de tu cono"); 
                                     op=JOptionPane.showInputDialog("Que sabor deseas tu Cono\n"
-                                            +"1.Fresa\n"
-                                            +"2.Vainilla\n"
-                                            +"3.Chocolate\n"
-                                            +"4.Caramelo\n"
-                                            +"5.Oreo\n"
+                                            +"1.Fresa ₡1000\n"
+                                            +"2.Vainilla ₡1000\n"
+                                            +"3.Chocolate ₡1000\n"
+                                            +"4.Caramelo ₡1000\n"
+                                            +"5.Oreo ₡1000\n"
                                             +"6.Salir\n");
                                   
                                   case 5: JOptionPane.showInternalMessageDialog(null, "Salir del menú"); break;  }   
